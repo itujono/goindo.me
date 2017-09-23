556381f2afc206f8860374bf8834b3c7e515880f
andhan <magicwarms@gmail.com>
2017-09-23 11:07:41 +0700

    DB terbaru ntar kita bikin yang online juga

--- goindo_me.sql
+++ goindo_me.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2017 at 03:17 PM
+-- Generation Time: Sep 23, 2017 at 06:06 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -23,29 +23,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `goindo_island`
+-- Table structure for table `goindo_city`
 --
 
-CREATE TABLE `goindo_island` (
-  `idISLAND` int(11) NOT NULL,
-  `nameISLAND` varchar(255) NOT NULL,
-  `populationISLAND` varchar(255) NOT NULL,
-  `densityISLAND` varchar(255) NOT NULL,
-  `areaISLAND` varchar(255) NOT NULL,
-  `capitalISLAND` varchar(255) NOT NULL,
-  `largestcityISLAND` varchar(255) NOT NULL,
-  `descISLAND` text NOT NULL,
-  `createdateISLAND` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `statusISLAND` int(1) NOT NULL DEFAULT '1'
+CREATE TABLE `goindo_city` (
+  `idCITY` int(11) NOT NULL,
+  `idPROVINCE` int(11) NOT NULL,
+  `nameCITY` varchar(255) NOT NULL,
+  `statusCITY` int(1) NOT NULL,
+  `createdateCITY` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedateCITY` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `goindo_island`
+-- Dumping data for table `goindo_city`
 --
 
-INSERT INTO `goindo_island` (`idISLAND`, `nameISLAND`, `populationISLAND`, `densityISLAND`, `areaISLAND`, `capitalISLAND`, `largestcityISLAND`, `descISLAND`, `createdateISLAND`, `statusISLAND`) VALUES
-(1, 'Kepulauan Riau', '1,500,000', '521 / km2', '1,801 km2', 'Tanjung Pinang', 'Batam', 'Comprised of over 3200 islands, Kepulauan Riau, Riau Islands, or Riau Archipelago is one of the younger and smaller Indonesia provinces; being incorporated as the 32nd province in September 2002. It is an attractive destination for tourists and travellers with the wide range of choices of accomodations, food and touring, recreation and entertainment facilities and welcoming people. Just a quick 40 minute ferry trip from Singapore, the two main islands of Bintan and Batam offer tourists a wide range of activities...', '2017-09-17 11:32:28', 1),
-(3, 'Makasar', '500,000', '400 / km2', '1992 km2', 'Sulawesi Tenggara', 'Kendari', 'Comprised of over 3200 islands, Makasar is one of the younger and smaller Indonesia provinces; being incorporated as the 32nd province in September 2002. It is an attractive destination for tourists and travellers with the wide range of choices of accomodations, food and touring, recreation and entertainment facilities and welcoming people.', '2017-09-17 12:58:25', 1);
+INSERT INTO `goindo_city` (`idCITY`, `idPROVINCE`, `nameCITY`, `statusCITY`, `createdateCITY`, `updatedateCITY`) VALUES
+(1, 1, 'Anambas Island', 1, '2017-09-23 03:54:00', '2017-09-23 03:56:01');
 
 -- --------------------------------------------------------
 
@@ -55,7 +50,7 @@ INSERT INTO `goindo_island` (`idISLAND`, `nameISLAND`, `populationISLAND`, `dens
 
 CREATE TABLE `goindo_more_desc` (
   `idDESC` int(11) NOT NULL,
-  `idISLAND` int(11) NOT NULL,
+  `idPROVINCE` int(11) NOT NULL,
   `titleDESC` varchar(255) NOT NULL,
   `moreDESC` varchar(255) NOT NULL,
   `createdateDESC` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -66,7 +61,7 @@ CREATE TABLE `goindo_more_desc` (
 -- Dumping data for table `goindo_more_desc`
 --
 
-INSERT INTO `goindo_more_desc` (`idDESC`, `idISLAND`, `titleDESC`, `moreDESC`, `createdateDESC`, `statusDESC`) VALUES
+INSERT INTO `goindo_more_desc` (`idDESC`, `idPROVINCE`, `titleDESC`, `moreDESC`, `createdateDESC`, `statusDESC`) VALUES
 (1, 1, 'Key Features', 'Kepulauan Riau is a highly attractive place for tourists and other travellers. On the main islands of Bintan and Batam, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:43:13', 1),
 (2, 1, 'How to Get There', 'Kepulauan Riau is a highly attractive place for tourists and other travellers. On the main islands of Bintan and Batam, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:44:12', 1),
 (3, 1, 'Attraction', 'Kepulauan Riau is a highly attractive place for tourists and other travellers. On the main islands of Bintan and Batam, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:51:33', 1),
@@ -74,6 +69,33 @@ INSERT INTO `goindo_more_desc` (`idDESC`, `idISLAND`, `titleDESC`, `moreDESC`, `
 (5, 3, 'Key Features', 'Makasar is a highly attractive place for tourists and other travellers. On the main islands of Sulawesi Tenggara, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:58:44', 1),
 (6, 3, 'How to Get There', 'Makasar is a highly attractive place for tourists and other travellers. On the main islands of Sulawesi Tenggara, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:59:01', 1),
 (7, 3, 'Attraction', 'Makasar is a highly attractive place for tourists and other travellers. On the main islands of Sulawesi Tenggara, there is a wealth of excellent hotels and resorts, shopping malls full of bargains, a vast array of restaurants, cafes...', '2017-09-17 12:59:22', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goindo_province`
+--
+
+CREATE TABLE `goindo_province` (
+  `idPROVINCE` int(11) NOT NULL,
+  `namePROVINCE` varchar(255) NOT NULL,
+  `populationPROVINCE` varchar(255) NOT NULL,
+  `densityPROVINCE` varchar(255) NOT NULL,
+  `areaPROVINCE` varchar(255) NOT NULL,
+  `capitalPROVINCE` varchar(255) NOT NULL,
+  `largestcityPROVINCE` varchar(255) NOT NULL,
+  `descPROVINCE` text NOT NULL,
+  `createdatePROVINCE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `statusPROVINCE` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `goindo_province`
+--
+
+INSERT INTO `goindo_province` (`idPROVINCE`, `namePROVINCE`, `populationPROVINCE`, `densityPROVINCE`, `areaPROVINCE`, `capitalPROVINCE`, `largestcityPROVINCE`, `descPROVINCE`, `createdatePROVINCE`, `statusPROVINCE`) VALUES
+(1, 'Kepulauan Riau', '1,500,000', '521 / km2', '1,801 km2', 'Tanjung Pinang', 'Batam', 'Comprised of over 3200 islands, Kepulauan Riau, Riau Islands, or Riau Archipelago is one of the younger and smaller Indonesia provinces; being incorporated as the 32nd province in September 2002. It is an attractive destination for tourists and travellers with the wide range of choices of accomodations, food and touring, recreation and entertainment facilities and welcoming people. Just a quick 40 minute ferry trip from Singapore, the two main islands of Bintan and Batam offer tourists a wide range of activities...', '2017-09-17 11:32:28', 1),
+(3, 'Makasar', '500,000', '400 / km2', '1992 km2', 'Sulawesi Tenggara', 'Kendari', 'Comprised of over 3200 islands, Makasar is one of the younger and smaller Indonesia provinces; being incorporated as the 32nd province in September 2002. It is an attractive destination for tourists and travellers with the wide range of choices of accomodations, food and touring, recreation and entertainment facilities and welcoming people.', '2017-09-17 12:58:25', 1);
 
 -- --------------------------------------------------------
 
@@ -100,16 +122,22 @@ INSERT INTO `goindo_users_admin` (`idADMIN`, `emailADMIN`, `passwordADMIN`, `cre
 --
 
 --
--- Indexes for table `goindo_island`
+-- Indexes for table `goindo_city`
 --
-ALTER TABLE `goindo_island`
-  ADD PRIMARY KEY (`idISLAND`);
+ALTER TABLE `goindo_city`
+  ADD PRIMARY KEY (`idCITY`);
 
 --
 -- Indexes for table `goindo_more_desc`
 --
 ALTER TABLE `goindo_more_desc`
   ADD PRIMARY KEY (`idDESC`);
+
+--
+-- Indexes for table `goindo_province`
+--
+ALTER TABLE `goindo_province`
+  ADD PRIMARY KEY (`idPROVINCE`);
 
 --
 -- Indexes for table `goindo_users_admin`
@@ -122,15 +150,20 @@ ALTER TABLE `goindo_users_admin`
 --
 
 --
--- AUTO_INCREMENT for table `goindo_island`
+-- AUTO_INCREMENT for table `goindo_city`
 --
-ALTER TABLE `goindo_island`
-  MODIFY `idISLAND` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `goindo_city`
+  MODIFY `idCITY` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `goindo_more_desc`
 --
 ALTER TABLE `goindo_more_desc`
   MODIFY `idDESC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `goindo_province`
+--
+ALTER TABLE `goindo_province`
+  MODIFY `idPROVINCE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `goindo_users_admin`
 --
